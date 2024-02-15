54391a5df6dd23f74e97c0406bf06197b861a8f5
Arup13455 <137142582+Arup13455@users.noreply.github.com>
2024-02-15 19:55:07 +0530

    Update printing transpose of a matrix.py

--- printing transpose of a matrix.py
+++ printing transpose of a matrix.py
@@ -2,7 +2,7 @@
 rows = int(input("Enter a num for rows: "))
 cols =	int(input("Enter a num for columns: "))
 # Get input for the matrices
-print("Enter elements for the first matrix:")
+print("Enter elements for the matrix:")
 matrix = [[int(input("Enter element at position ({},{}) : ".format(i+1, j+1))) for j in range(cols)] for i in range(rows)]
 # transposing the matrix
 trans = [[0 for i in range(rows)]for j in range(cols)]

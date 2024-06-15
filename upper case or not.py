e8e100e177ce630ee7db019401ede485c258a037
Arup13455 <137142582+Arup13455@users.noreply.github.com>
2024-06-15 11:32:09 +0530

    Delete upper case or not.py

--- upper case or not.py
+++
@@ -1,7 +0,0 @@
-characters = str(input("Enter character: "))
-if 'A' <= characters <= 'Z':
-    print("Upper case")
-elif 'a' <= characters <= 'z':
-    print("Lower case")
-else:
-    print("Not english letter")

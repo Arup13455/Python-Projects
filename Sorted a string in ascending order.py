d974bda3a106bab5c2b03945191826757d107cbb
Arup13455 <137142582+Arup13455@users.noreply.github.com>
2024-06-15 11:29:33 +0530

    Delete Sorted a string in ascending order.py

--- Sorted a string in ascending order.py
+++
@@ -1,8 +0,0 @@
-# Get user input for the string
-user_input = input("Enter a string: ")
-
-# Sort the string in ascending order
-sorted_string = ''.join(sorted(user_input))
-
-# Display the sorted string
-print("Sorted string in ascending order:",sorted_string)

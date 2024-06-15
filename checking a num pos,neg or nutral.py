393e4905616cf2e3232617890cf734aa3352c35e
Arup13455 <137142582+Arup13455@users.noreply.github.com>
2024-06-15 11:30:32 +0530

    Delete checking a num pos,neg or nutral.py

--- checking a num pos,neg or nutral.py
+++
@@ -1,8 +0,0 @@
-# program to check if a num is positive,negative or zero
-num = int(input("Enter a num: "))
-if num > 0:
-    print(f"{num} is  a positive num.")
-elif num == 0:
-    print("It is zero.")
-else:
-    print(f"{num} is a negative num.")

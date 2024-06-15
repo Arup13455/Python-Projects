2e3e18b399b7f0a9cf0652b7664421056131b4eb
Arup13455 <137142582+Arup13455@users.noreply.github.com>
2024-06-15 11:31:55 +0530

    Delete word  replacement program.py

--- word  replacement program.py
+++
@@ -1,8 +0,0 @@
-def replace_word():
-    string = "hi guys, I am Arun, and hi hi hi hi"
-    word_to_replace = input("Enter the word to replace: ")
-    word_replacement = input("Enter the word replacement: ")
-    print(string.replace(word_to_replace, word_replacement))
-
-
-replace_word()

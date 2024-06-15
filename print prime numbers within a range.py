99e4de88e6076d800481063378ff682328548413
Arup13455 <137142582+Arup13455@users.noreply.github.com>
2024-06-15 11:38:41 +0530

    Delete print prime numbers within a range.py

--- print prime numbers within a range.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a num: "))
-for i in range(1,n+1):
-    count = 0
-    for j in range(2,int(i/2)+1):
-        if i % j == 0:
-            count += 1
-            break
-    if count == 0 and i != 1:
-        print(i)

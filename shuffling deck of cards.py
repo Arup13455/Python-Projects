cc69fdcbc310f05a1d725a1e4a2acff4d3387ff2
Arup13455 <137142582+Arup13455@users.noreply.github.com>
2024-06-15 11:35:04 +0530

    Delete shuffling deck of cards.py

--- shuffling deck of cards.py
+++
@@ -1,7 +0,0 @@
-# program to shuffle deck of cards
-import random, itertools
-deck = list(itertools.product(range(1,14),["spade","club","Hearts","Diamond"]))
-random.shuffle(deck)
-num = int(input("Enter a num of times you want to shuffle: "))
-for i in range(num):
-        print(deck[i][0],"of",deck[i][1])
